b0f6e4d77b7744fe26e58b730d1515323518f191
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-08-07 22:46:18 -0300

    project structure

---
+++ src/main/java/com/addressmanager/Api_Address/ApiAddressApplication.java
@@ -0,0 +1,13 @@
+package com.addressmanager.Api_Address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addressmanager/Api_Address/ApiAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressmanager.Api_Address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
